644e47c3fe37f8977709b981e44ef2aafbee43b2
1burhanuddin <111burhanuddin@gmail.com>
2024-09-22 10:39:12 +0530

    url /get solved

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
-app.get("/A", (req, res) => {
-  res.send("Hello World!")
-})
+
 
 app.post("/", async (req, res) => {
   const { username } = req.body;
@@ -32,7 +30,34 @@ app.post("/", async (req, res) => {
 
   }
 
-  return res.json({ username: username, secret: "sha256..." });
+ 
 });
+
+app.get("/", async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    const r = await axios.put(
+        "https://api.chatengine.io/users/",
+        {
+            username: username , secret: username, first_name: username
+        },
+        {
+            headers:{"PRIVATE-KEY": "1c9cad88-d5e7-4696-9396-8ea541d364da"}
+        }
+
+    )
+    return res.status(r.status).json(r.data)
+
+  } catch (e) {
+    
+    return res.status(e.responce.status).json(e.responce.data)
+
+  }
+
+ 
+});
+
+
 
 app.listen(3001);
